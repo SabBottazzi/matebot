3cdbe03794cadcc6ffa166f4335424d52d3a80df
SabBottazzi <sabrinabottazzi@gmail.com>
2020-12-04 13:34:00 -0300

    Cambios en el msj de bienvenida

--- src/modules/welcome.py
+++ src/modules/welcome.py
@@ -19,6 +19,7 @@ class Welcome(commands.Cog):
         \nTe cuento, somos una comunidad de personas interesadas en tecnología y ciencias informáticas.\nCharlamos sobre lenguajes de programación, diseño web, infraestructura, compartimos dudas y tratamos de resolverlas juntxs.\nAdemás, nos organizamos en grupos para estudiar, hacer proyectos juntos y charlar en inglés para perfeccionarnos.\nTenemos un espacio de coworking, también nos vamos de after office y jugamos jueguitos!
         \nAcá te dejamos información que **es necesario que revises antes de comenzar a participar**, ya que es muy importante que todxs contribuyamos a mantener este server como un espacio seguro, amigable y divertido para cualquier persona que participe.
         \n* Código de conducta <#748183026244255824>\n* Manual de uso <#747925827265495111>
+        \n**Configuración de Mensajes directos dentro del servidor:**\nTe recomendamos que sigas estas instrucciones para configurarlos: <https://bit.ly/3qmOPTr>
         \nNos encantaría que pases por <#748547143157022871> y nos cuentes algo de vos :slight_smile:
         \nSaludos!
         \n*El Staff de FrontendCafé*''')
